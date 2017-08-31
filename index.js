96e56714a9c6542c46164b49ead4def11624c48f
Will Bruce <willtiger13@gmail.com>
2017-08-31 16:45:32 -0400

    added link back to home page on edit form page

--- index.js
+++ index.js
@@ -18,28 +18,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 
-// app.get('/', (req, res) =>{
-//   res.render('index', {todos: todos});
-// });
-//
-// app.post('/add', (req, res) => {
-//   todos.push({task: req.body.newTask, done: false});
-//   res.redirect('/');
-// });
-//
-//
-// app.post('/completed', (req, res) => {
-//   todos.forEach((item, index) => {
-//     if (req.body.done === todos[index].task){
-//       item.done = true;
-//       console.log('clicked', item);
-//       console.log('clicked todos', todos);
-//     }
-//   });
-//   res.redirect('/');
-// });
-
-
 
 
 router(app);
